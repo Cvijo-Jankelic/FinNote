6219677883b27e49f2d5dcceefc9731d21960d48
Cvijo-Jankelic <c.jankelic@gmail.com>
2024-11-27 16:53:35 +0100

    Adding a hcontroller in right directory

--- src/main/java/com/project/finnote/controllers/HelloController.java
+++ src/main/java/com/project/finnote/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.project.finnote;
+package com.project.finnote.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.project.finnote {
 
     opens com.project.finnote to javafx.fxml;
     exports com.project.finnote;
+    exports com.project.finnote.controllers;
+    opens com.project.finnote.controllers to javafx.fxml;
 }
